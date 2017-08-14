d5b01b85abfd97c7353a6929ae015d41b6fa351a
CacaoRick <cacaorick@gmail.com>
2017-08-14 14:49:06 +0800

    根據是否有 tsconfig.json 來判斷要建立 tsx 或 js

--- generators/component/index.js
+++ generators/component/index.js
@@ -3,16 +3,45 @@ const Generator = require("yeoman-generator")
 module.exports = class extends Generator {
   constructor(args, options) {
     super(args, options)
+    const tsconfig = this.fs.readJSON(this.destinationPath("tsconfig.json"))
 
-    this.argument("componentName", { type: String, required: true })
+    this.argument("componentName", { type: String, required: false })
+    this.argument("typescript", { type: Boolean, required: false, default: tsconfig ? true : false })
+  }
+
+  prompting() {
+    const prompts = []
+    if (!this.options.componentName) {
+      prompts.push({
+        type: "input",
+        name: "componentName",
+        message: "What's your component name:",
+      })
+    }
+
+    return this.prompt(prompts)
+      .then((answers) => {
+        if (answers.componentName) {
+          this.options.componentName = answers.componentName
+        }
+      })
   }
 
   writing() {
     let componentName = this.options.componentName;
-    this.fs.copyTpl(
-      this.templatePath("index.js"),
-      this.destinationPath("src/components/" + componentName + ".js"),
-      { componentName: componentName }
-    )
+
+    if (this.options.typescript) {
+      this.fs.copyTpl(
+        this.templatePath("index.tsx"),
+        this.destinationPath("src/components/" + componentName + ".tsx"),
+        { componentName: componentName }
+      )
+    } else {
+      this.fs.copyTpl(
+        this.templatePath("index.js"),
+        this.destinationPath("src/components/" + componentName + ".js"),
+        { componentName: componentName }
+      )
+    }
   }
 }
--- generators/container/index.js
+++ generators/container/index.js
@@ -3,16 +3,45 @@ const Generator = require("yeoman-generator")
 module.exports = class extends Generator {
   constructor(args, options) {
     super(args, options)
+    const tsconfig = this.fs.readJSON(this.destinationPath("tsconfig.json"))
 
-    this.argument("containerName", { type: String, required: true })
+    this.argument("containerName", { type: String, required: false })
+    this.argument("typescript", { type: Boolean, required: false, default: tsconfig ? true : false })
+  }
+
+  prompting() {
+    const prompts = []
+    if (!this.options.componentName) {
+      prompts.push({
+        type: "input",
+        name: "containerName",
+        message: "What's your component name:",
+      })
+    }
+
+    return this.prompt(prompts)
+      .then((answers) => {
+        if (answers.containerName) {
+          this.options.containerName = answers.containerName
+        }
+      })
   }
 
   writing() {
     let containerName = this.options.containerName;
-    this.fs.copyTpl(
-      this.templatePath("index.js"),
-      this.destinationPath("src/containers/" + containerName + "/index.js"),
-      { containerName: containerName }
-    )
+
+    if (this.options.typescript) {
+      this.fs.copyTpl(
+        this.templatePath("index.tsx"),
+        this.destinationPath("src/containers/" + containerName + "/index.tsx"),
+        { containerName: containerName }
+      )
+    } else {
+      this.fs.copyTpl(
+        this.templatePath("index.js"),
+        this.destinationPath("src/containers/" + containerName + "/index.js"),
+        { containerName: containerName }
+      )
+    }
   }
 }
