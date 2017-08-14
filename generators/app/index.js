e9863119c22c61c392dd9b1dd0c64c7821357f77
CacaoRick <cacaorick@Rick-MBPR.local>
2017-08-14 09:53:04 +0800

    根據資料夾搬動，改寫 app 的 generator，使用者輸入改成用 checkbox，並加入 typescript 選項

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,40 +5,61 @@ module.exports = class extends Generator {
   constructor(args, options) {
     super(args, options)
 
-    this.argument("appName", { type: String, required: true })
+    this.argument("appname", { type: String, required: false })
+    this.argument("expo", { type: Boolean, required: false, default: false })
+    this.argument("typescript", { type: Boolean, required: false, default: false })
   }
 
   prompting() {
     const EXPO = "Expo (create-react-native-app)"
     const STANDARD = "Standard (react-native-cli)"
-    return this.prompt([{
-      type: "list",
-      name: "projectType",
-      message: "React Native project type",
-      choices: [EXPO, STANDARD],
-    }]).then((answers) => {
-      // 根據專案類型複製 entryPoint
-      if (answers.projectType === EXPO) {
-        this.fs.copy(
-          this.templatePath("App.js"),
-          this.destinationPath("App.js")
-        )
-      } else {
-        this.fs.copy(
-          this.templatePath("index.android.js"),
-          this.destinationPath("index.android.js"),
-          { appName: this.options.appName }
-        )
-        this.fs.copy(
-          this.templatePath("index.ios.js"),
-          this.destinationPath("index.ios.js"),
-          { appName: this.options.appName }
-        )
-      }
+
+    const prompts = []
+    if (!this.options.appname) {
+      prompts.push({
+        type: "input",
+        name: "appname",
+        message: "Project name",
+      })
+    }
+
+    prompts.push({
+      type: "checkbox",
+      name: "options",
+      message: "Select options for your project",
+      choices: ["expo", "typescript"],
     })
+    return this.prompt(prompts)
+      .then((answers) => {
+        this.options.appname = answers.appname
+        answers.options.forEach((option) => {
+          if (option === "expo") {
+            this.options.expo = true
+          }
+          if (option === "typescript") {
+            this.options.typescript = true
+          }
+        });
+        console.log(this.options)
+      })
   }
 
-  installDependencies() {
+  configuring() {
+    this.config.set(typescript, this.options.typescript)
+    this.config.save()
+  }
+
+  writing() {
+    if (this.options.typescript) {
+      this._installDependencies()
+      this._constructFileStruct()
+    } else {
+      this._installTypescriptDependencies()
+      this._constructTypescriptFileStruct()
+    }
+  }
+
+  _installDependencies() {
     this.yarnInstall([
       "immutable",
       "prop-types",
@@ -50,16 +71,39 @@ module.exports = class extends Generator {
     ])
   }
 
-  constructFileStruct() {
+  _installTypescriptDependencies() {
+
+  }
+
+  _constructFileStruct() {
+    // 複製 enterpoint
+    if (this.options.expo) {
+      this.fs.copy(
+        this.templatePath("babel/App.js"),
+        this.destinationPath("App.js")
+      )
+    } else {
+      this.fs.copy(
+        this.templatePath("babel/index.android.js"),
+        this.destinationPath("index.android.js"),
+        { appname: this.options.appname }
+      )
+      this.fs.copy(
+        this.templatePath("babel/index.ios.js"),
+        this.destinationPath("index.ios.js"),
+        { appname: this.options.appname }
+      )
+    }
+
     // 複製 src 資料夾
     this.fs.copy(
-      this.templatePath("src"),
+      this.templatePath("babel/src"),
       this.destinationPath("src")
     )
 
     // 複製 eslint confit
     this.fs.copy(
-      this.templatePath(".eslintrc"),
+      this.templatePath("babel/.eslintrc"),
       this.destinationPath(".eslintrc")
     )
 
@@ -68,5 +112,31 @@ module.exports = class extends Generator {
     mkdirp("src/images")
     mkdirp("src/lib")
     mkdirp("src/models")
+  }
+
+  _constructTypescriptFileStruct() {
+    if (this.options.expo) {
+      this.fs.copy(
+        this.templatePath("typescript/App.js"),
+        this.destinationPath("App.js")
+      )
+    } else {
+      this.fs.copy(
+        this.templatePath("typescript/index.android.js"),
+        this.destinationPath("index.android.js"),
+        { appname: this.options.appname }
+      )
+      this.fs.copy(
+        this.templatePath("typescript/index.ios.js"),
+        this.destinationPath("index.ios.js"),
+        { appname: this.options.appname }
+      )
+    }
+
+    // 複製 src 資料夾
+    this.fs.copy(
+      this.templatePath("typescript/src"),
+      this.destinationPath("src")
+    )
   }
 }
