14b18239ec990c75b207515bb2a24895cf301bcd
CacaoRick <cacaorick@gmail.com>
2017-08-14 15:28:15 +0800

    改用 exists 判斷 tsconfig 是否存在，不用讀取了

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,47 +1,46 @@
 const Generator = require("yeoman-generator")
 
 module.exports = class extends Generator {
-  constructor(args, options) {
-    super(args, options)
-    const tsconfig = this.fs.readJSON(this.destinationPath("tsconfig.json"))
+	constructor(args, options) {
+		super(args, options)
 
-    this.argument("componentName", { type: String, required: false })
-    this.argument("typescript", { type: Boolean, required: false, default: tsconfig ? true : false })
-  }
+		this.argument("componentName", { type: String, required: false })
+		this.argument("typescript", { type: Boolean, required: false, default: this.fs.exists(this.destinationPath("tsconfig.json")) })
+	}
 
-  prompting() {
-    const prompts = []
-    if (!this.options.componentName) {
-      prompts.push({
-        type: "input",
-        name: "componentName",
-        message: "What's your component name:",
-      })
-    }
+	prompting() {
+		const prompts = []
+		if (!this.options.componentName) {
+			prompts.push({
+				type: "input",
+				name: "componentName",
+				message: "What's your component name:",
+			})
+		}
 
-    return this.prompt(prompts)
-      .then((answers) => {
-        if (answers.componentName) {
-          this.options.componentName = answers.componentName
-        }
-      })
-  }
+		return this.prompt(prompts)
+			.then((answers) => {
+				if (answers.componentName) {
+					this.options.componentName = answers.componentName
+				}
+			})
+	}
 
-  writing() {
-    let componentName = this.options.componentName;
+	writing() {
+		let componentName = this.options.componentName;
 
-    if (this.options.typescript) {
-      this.fs.copyTpl(
-        this.templatePath("index.tsx"),
-        this.destinationPath("src/components/" + componentName + ".tsx"),
-        { componentName: componentName }
-      )
-    } else {
-      this.fs.copyTpl(
-        this.templatePath("index.js"),
-        this.destinationPath("src/components/" + componentName + ".js"),
-        { componentName: componentName }
-      )
-    }
-  }
+		if (this.options.typescript) {
+			this.fs.copyTpl(
+				this.templatePath("index.tsx"),
+				this.destinationPath("src/components/" + componentName + ".tsx"),
+				{ componentName: componentName }
+			)
+		} else {
+			this.fs.copyTpl(
+				this.templatePath("index.js"),
+				this.destinationPath("src/components/" + componentName + ".js"),
+				{ componentName: componentName }
+			)
+		}
+	}
 }
