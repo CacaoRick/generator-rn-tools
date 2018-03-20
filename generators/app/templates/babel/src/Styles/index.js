522d8c19e889b159e591d4cf7500fc459913b71f
CacaoRick <cacaorick@gmail.com>
2018-03-20 17:19:58 +0800

    加入 Styles/index.js

---
+++ generators/app/templates/babel/src/Styles/index.js
@@ -0,0 +1,18 @@
+import Colors from "./Colors"
+
+export default {
+	flexContainer: {
+		flex: 1,
+	},
+	shadow: {
+		elevation: 2,
+		shadowColor: Colors.Black,
+		shadowOffset: { width: 0, height: 0.5 },
+		shadowOpacity: 0.3,
+	},
+	separator: {
+		paddingLeft: 16,
+		borderBottomWidth: 0.5,
+		backgroundColor: Colors.LightDivider,
+	},
+}
