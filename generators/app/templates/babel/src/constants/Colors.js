f42172cfbd12e53f345f6e37f6f13613039bd778
CacaoRick <cacaorick@gmail.com>
2018-03-20 17:18:42 +0800

    Colors 做成 Object

--- generators/app/templates/babel/src/constants/Colors.js
+++ generators/app/templates/babel/src/constants/Colors.js
@@ -1,31 +1,21 @@
-// React Native 內建顏色 https://facebook.github.io/react-native/docs/colors.html#named-colors
-export const White = "white"
-export const Black = "black"
-export const Red = "red"
-export const Green = "green"
-export const Yellow = "yellow"
-export const Orange = "orange"
-export const LightGray = "lightgray"
-export const Transparent = "transparent"		// 透明
-
-// IOS Color https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
-export const IosRed = "rgb(255, 59, 48)"
-export const IosOrange = "rgb(255, 149, 0)"
-export const IosYellow = "rgb(255, 204, 0)"
-export const IosGreen = "rgb(76, 217, 100)"
-export const IosTealBlue = "rgb(90, 200, 250)"
-export const IosBlue = "rgb(0, 122, 255)"
-export const IosPurple = "rgb(88, 86, 214)"
-export const IosPink = "rgb(255, 45, 85)"
-
-// Material Design Color https://material.io/guidelines/style/color.html
-// Dark Text
-export const DarkPrimaryText = "rgba(0, 0, 0, 0.87)"			// 87% Black
-export const DarkSecondaryryText = "rgba(0, 0, 0, 0.54)"	// 54% Black
-export const DarkDisabledText = "rgba(0, 0, 0, 0.38)"		// 38% Black
-export const DarkDivider = "rgba(0, 0, 0, 0.12)"					// 12% Black
-// Light Text
-export const LightPrimaryText = "rgba(255, 255, 255, 1.0)" 		  // 100% White
-export const LightSecondaryryText = "rgba(255, 255, 255, 0.7)"   // 70% White
-export const LightDisabledText = "rgba(255, 255, 255, 0.5)"      // 50% White
-export const LightDivider = "rgba(255, 255, 255, 0.12)"          // 12% White
+export default {
+	White: "white",
+	Black: "black",
+	Primary: "#455A64",
+	LightPrimary: "#90A4AE",
+	DarkPrimary: "#263238",
+	Accent: "#FFC107",
+	LightPrimaryText: "rgba(255, 255, 255, 1.0)",
+	LightSecondaryText: "rgba(255, 255, 255, 0.7)",
+	LightDisabledText: "rgba(255, 255, 255, 0.5)",
+	LightDivider: "rgba(255, 255, 255, 0.12)",
+	DarkPrimaryText: "rgba(0, 0, 0, 0.87)",
+	DarkSecondaryryText: "rgba(0, 0, 0, 0.54)",
+	DarkDisabledText: "rgba(0, 0, 0, 0.38)",
+	DarkDivider: "rgba(0, 0, 0, 0.12)",
+	Transparent: "transparent",
+	transparentBlack20: "rgba(0, 0, 0, 0.2)",
+	transparentBlack40: "rgba(0, 0, 0, 0.4)",
+	transparentBlack60: "rgba(0, 0, 0, 0.6)",
+	transparentBlack80: "rgba(0, 0, 0, 0.8)",
+}
