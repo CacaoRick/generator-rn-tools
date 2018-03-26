ff9f2223cce24b510b795ca25c0157b29e2c217d
CacaoRick <cacaorick@gmail.com>
2018-03-26 15:06:14 +0800

    Add navbar padding to root scene.

--- generators/app/templates/src/containers/AppRouter/index.js
+++ generators/app/templates/src/containers/AppRouter/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
+import { StyleSheet, Platform } from "react-native"
 import { Router, Scene } from "react-native-router-flux"
 import Main from "../Main"
 
@@ -8,13 +9,23 @@ class AppRouter extends React.Component {
 	render() {
 		return (
 			<Router>
-				<Scene key="root">
+				<Scene key="root" style={styles.paddingNavBar}>
 					<Scene key="Main" component={Main} title="Main" initial />
 				</Scene>
 			</Router>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	paddingNavBar: {
+		paddingTop: Platform.select({
+			ios: 64,
+			android: 54,
+		}),
+	},
+})
+
 
 const mapStateToProps = (state) => {
 	
